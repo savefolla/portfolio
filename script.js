df22ea44e13edec54a8aff915d01f39a5188261c
Saverio Follador <saverio@moku.io>
2019-04-13 17:29:08 +0200

    content

--- script.js
+++ script.js
@@ -33,3 +33,13 @@ const animate = () => {
 };
 
 animate();
+
+const getDays = () => {
+  const birthdate = new Date(1995, 12, 25).getTime();
+  const today = new Date().getTime();
+  const age = (today - birthdate) / 1000 / 60 / 60 / 24 / 365;
+  document.getElementById('age').innerHTML = age.toFixed(0);
+  console.log(age);
+};
+
+getDays();
