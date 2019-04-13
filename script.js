fe1bc8388ce8957fcac0730114bbb44462e7bc6e
Saverio Follador <saverio@moku.io>
2019-04-13 18:03:04 +0200

    mouse move

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const animate = () => {
 
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
   if (index % 50 === 0) {
     sphere.geometry = new THREE.SphereGeometry(radius, 25 * Math.random(), 25 * Math.random(), 0, Math.PI * 2, 10 * Math.random(), 10 * Math.random());
@@ -45,7 +45,13 @@ const getDays = () => {
   const today = new Date().getTime();
   const age = (today - birthdate) / 1000 / 60 / 60 / 24 / 365;
   document.getElementById('age').innerHTML = age.toFixed(0);
-  console.log(age);
 };
 
 getDays();
+
+addEventListener('mousemove', (e) => {
+  const xFactor = e.clientX / window.innerWidth - .5;
+  const yFactor = e.clientY / window.innerHeight - .5;
+  camera.position.x = xFactor;
+  camera.position.y = yFactor;
+});
