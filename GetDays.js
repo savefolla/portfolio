248ae254cca99bd83d34235ee3964dd63fc04791
Saverio Follador <saverio@moku.io>
2019-07-23 15:30:12 +0200

    minor fix

--- GetDays.js
+++ GetDays.js
@@ -2,7 +2,7 @@ const getDays = () => {
   const birthdate = new Date(1995, 12, 25).getTime();
   const today = new Date().getTime();
   const age = (today - birthdate) / 1000 / 60 / 60 / 24 / 365;
-  document.getElementById('age').innerHTML = age.toFixed(0);
+  document.getElementById('age').innerHTML = Math.floor(age);
 };
 
 getDays();
