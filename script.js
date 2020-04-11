3cae54d0153841981322e2c9ce447fcc5a9c451a
Saverio Follador <saverio@moku.io>
2020-04-11 20:31:03 +0200

    minor

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ const init = () => {
   };
 
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight + 150), 0.1, 1000);
   camera.position.z = 5;
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, (window.innerHeight + 150));
   document.body.appendChild(renderer.domElement);
 
   const radius = 2;
